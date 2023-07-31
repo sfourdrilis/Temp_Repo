ef2ecf4b1c6f4069876de7a7c294c344c98e7db8
Saskia Jurriaans <s.jurriaans@aims.gov.au>
2023-07-31 14:20:12 +1000

    I changed some lines of script

--- analysis.R
+++ analysis.R
@@ -3,3 +3,11 @@ y <- 40*2 + rnorm(n = 10, mean = 0, sd = 5)
 plot(x = x, y = y)
 x |> summary()
 x |> median()
+
+## I make some changes to Sevs code
+
+x |> max()
+x |> min()
+
+test
+blablabla test
